2e789d5c0ecec59f8726e02769e2ee4b56da5502
osamamagar <osamamagar99@gmail.com>
2023-10-19 10:52:18 +0300

    Updates

---
+++ static/js/574.c2697c3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_project_movieapp=self.webpackChunkreact_project_movieapp||[]).push([[574],{9574:function(e,r,c){c.r(r),c.d(r,{default:function(){return t}});c(2791);var n=c(184);function t(){return(0,n.jsxs)("div",{className:"text-center my-5",children:[(0,n.jsx)("h1",{children:"404"}),(0,n.jsx)("h2",{children:"Sorry - We can't find your page "})]})}}}]);
+//# sourceMappingURL=574.c2697c3b.chunk.js.map
